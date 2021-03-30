ec5d5b104a0f9506af69d06e84cb1ad1bdcdb8a7
apo6377 <apo6377@rit.edu>
2021-03-29 22:42:43 -0400

    Created files for I2C controller and IMU, constrained some I/O

---
+++ urc_rover/src/hdl/I2CMaster.sv
@@ -0,0 +1,28 @@
+//////////////////////////////////////////////////////////////////////////////////
+// Company: RIT SPEX
+// Engineer: Alexander Olds
+// 
+// Create Date: 03/29/2021 10:07:42 PM
+// Design Name: 
+// Module Name: I2CMaster
+// Project Name: urc_rover
+// Target Devices: Artix 7 35T
+// Tool Versions: Vivado 2020.2
+// Description: I2C Master module
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module I2CMaster #(
+    parameter SYSCLK_FREQ = 100_000_000
+) (
+    input sclk,
+    input rstn
+);
+    
+endmodule
---
+++ urc_rover/src/hdl/LSM9DS1.sv
@@ -0,0 +1,28 @@
+//////////////////////////////////////////////////////////////////////////////////
+// Company: RIT SPEX
+// Engineer: Alexander Olds
+// 
+// Create Date: 03/29/2021 10:07:42 PM
+// Design Name: 
+// Module Name: LSM9DS1
+// Project Name: urc_rover
+// Target Devices: Artix 7 35T
+// Tool Versions: Vivado 2020.2
+// Description: LSM9DS1 IMU Driver & interpreter
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module LSM9DS1 #(
+    parameter SYSCLK_FREQ = 100_000_000
+) (
+    input sclk,
+    input rstn
+);
+    
+endmodule
