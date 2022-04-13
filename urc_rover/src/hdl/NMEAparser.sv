24b020941eeb8cdec4fb2710fae85608cdbd93ba
Alex Olds <44590110+apo6377@users.noreply.github.com>
2022-04-13 16:58:09 -0400

    Added state machine, FORMAT state example

--- urc_rover/src/hdl/NMEAparser.sv
+++ urc_rover/src/hdl/NMEAparser.sv
@@ -45,7 +45,7 @@ module NMEAparser #(
     input sclk,
     input rstn,
 
-    input [7:0] dataString, 
+    input [7:0] dataByte, 
     input       dataReady,
 
     GPSdata gpsResults,
@@ -72,111 +72,167 @@ module NMEAparser #(
     //logic [ 3:0] altMSL      [7:0]; //14
     logic olst, st;
 
+    logic [4:0] index;
+
+    enum logic[15:0] { IDLE, FORMAT, TIMESTAMP, LATITUDE, NS, LONGITUDE } dataBlock;
+
     always_ff @(posedge sclk ) begin : parser
-        //check if reset (active low)
-        if ((!rstn) || (st == 99)) begin
-            GPSReady <= 0;
-            gpgga <= 0;
-            utc <= 0;
-            latitude <= 0;
-            ns <= 0;
-            longitude <= 0;
-            ew <= 0;
-            quality <= 0;
-            numSats <= 0;
-            hdop <= 0;
-            geoid <= 0;
-            metersH <= 0;
-            geoidal <= 0;
-            metersG <= 0;
-            age <= 0;
-            cSum <= 0;
-            st <= 1;
-        end 
-        //check for $
-        else if (!(dataString ^ 8'b00100100)) begin
-            GPSReady <= 0;
-            st <= 2;
-        end 
-        //validate GPGGA after $
-        else if ((st >= 2) && (st < 7)) begin
-            if ((st == 2) && (!(dataString ^ 8'b01000111))) begin
-                gpgga[5] <= dataString;
-                st <= 3;
-            end else if ((st == 3) && (!(dataString ^ 8'b01010000))) begin
-                gpgga[4] <= dataString;
-                st <= 4;
-            end else if ((st == 4) && (!(dataString ^ 8'b01000111))) begin
-                gpgga[3] <= dataString;
-                st <= 5;
-            end else if ((st == 5) && (!(dataString ^ 8'b01000111))) begin
-                gpgga[2] <= dataString;
-                st <= 6;
-            end else if ((st == 6) && (!(dataString ^ 8'b01000001))) begin
-                gpgga[1] <= dataString;
-                st <= 7;
-            end else begin
-                st <= 99;
+        case (dataBlock)
+            IDLE: begin
+                
             end
-        end
-        //Parse
-        else if (st >= 7) begin
-            //validate ,'s are where expected
-            if ((st == 7) || (st == 18) || (st == 28) || (st == 30) || (st == 41) || (st == 43) || (st == 45) || (st == 47) || (st == 52) || (st == 57) || (st == 59) || (st == 64) || (st == 66) || (st == 70)) begin
-                if (!(dataString ^ 8'b00101100)) begin
-                    olst = st;
-                    st++;
-                    if (st == 7) begin
-                        gpgga[0] = dataString;
+
+            FORMAT: begin
+                if (dataReady) begin //ONLY WORKS IF DATAREADY IS HIGH FOR ONLY 1 CLOCK CYCLE
+                    if (index >= 5) begin
+                        if (gpgga == /*gpgga ASCII*/1) begin
+                            if (dataByte == /*comma ASCII*/1) begin
+                                index <= 0;
+                                dataBlock <= TIMESTAMP;
+                            end else begin
+                                index <= 0;
+                                dataBlock <= IDLE;
+                            end
+                        end else begin
+                            index <= 0;
+                            dataBlock <= IDLE;
+                        end
                     end
+                    gpgga <= {gpgga[4:0], dataByte};
+                    index <= index + 1;
+
                 end else begin
-                    st = 99;
-                end
-            //fill arrays with values
-            end else if (st < 82) begin
-                if ((st >= 8) && (st < 18)) begin
-                    utc[17-st] = dataString;
-                end else if ((st >= 19) && (st < 28)) begin
-                    latitude[27-st] = dataString;
-                end else if ((st >= 29) && (st < 30)) begin
-                    ns[29-st] = dataString;
-                end else if ((st >= 31) && (st < 41)) begin
-                    longitude[40-st] = dataString;
-                end else if ((st >= 42) && (st < 43)) begin
-                    ew[42-st] = dataString;
-                end else if ((st >= 44) && (st < 45)) begin
-                    quality[44-st] = dataString;
-                end else if ((st >= 46) && (st < 47)) begin
-                    numSats[46-st] = dataString;
-                end else if ((st >= 48) && (st < 52)) begin
-                    hdop[51-st] = dataString;
-                end else if ((st >= 53) && (st < 57)) begin
-                    geoid[56-st] = dataString;
-                end else if ((st >= 58) && (st < 59)) begin
-                    metersH[58-st] = dataString;
-                end else if ((st >= 60) && (st < 64)) begin
-                    geoidal[63-st] = dataString;
-                end else if ((st >= 65) && (st < 66)) begin
-                    metersG[65-st] = dataString;
-                end else if ((st >= 67) && (st < 70)) begin
-                    age[69-st] = dataString;
-                end else if ((st >= 71) && (st < 81)) begin
-                    checksum[80-st] = dataString;
-                st++;
-                //Checksum
-                end else begin
-                    cSum = gpgga ^ utc ^ latitude ^ ns ^ longitude ^ ew ^ quality ^ numSats ^ dilution ^ altitude ^ metersH ^ geoidal ^ metersG ^ age;
-                    //data ready
-                    if (!(cSum ^ checksum)) begin
-                        GPSReady <= 1;
-                    end else begin
-                        GPSReady <= 0;
-                        st <= 99;
-                    end
+                    dataBlock <= FORMAT;
                 end
             end
-        end
+
+            TIMESTAMP: begin
+                
+            end
+
+            LONGITUDE: begin
+                
+            end
+
+            NS: begin
+                
+            end
+
+            LATITUDE: begin
+                
+            end
+            default: begin
+                datablock <= FORMAT;
+            end
+        endcase
+
     end
+
+    // always_ff @(posedge sclk ) begin : parser
+    //     //check if reset (active low)
+    //     if ((!rstn) || (st == 99)) begin
+    //         GPSReady <= 0;
+    //         gpgga <= 0;
+    //         utc <= 0;
+    //         latitude <= 0;
+    //         ns <= 0;
+    //         longitude <= 0;
+    //         ew <= 0;
+    //         quality <= 0;
+    //         numSats <= 0;
+    //         hdop <= 0;
+    //         geoid <= 0;
+    //         metersH <= 0;
+    //         geoidal <= 0;
+    //         metersG <= 0;
+    //         age <= 0;
+    //         cSum <= 0;
+    //         st <= 1;
+    //     end 
+    //     //check for $
+    //     else if (!(dataByte ^ 8'b00100100)) begin
+    //         GPSReady <= 0;
+    //         st <= 2;
+    //     end 
+    //     //validate GPGGA after $
+    //     else if ((st >= 2) && (st < 7)) begin
+    //         if ((st == 2) && (!(dataByte ^ 8'b01000111))) begin
+    //             gpgga[5] <= dataByte;
+    //             st <= 3;
+    //         end else if ((st == 3) && (!(dataByte ^ 8'b01010000))) begin
+    //             gpgga[4] <= dataByte;
+    //             st <= 4;
+    //         end else if ((st == 4) && (!(dataByte ^ 8'b01000111))) begin
+    //             gpgga[3] <= dataByte;
+    //             st <= 5;
+    //         end else if ((st == 5) && (!(dataByte ^ 8'b01000111))) begin
+    //             gpgga[2] <= dataByte;
+    //             st <= 6;
+    //         end else if ((st == 6) && (!(dataByte ^ 8'b01000001))) begin
+    //             gpgga[1] <= dataByte;
+    //             st <= 7;
+    //         end else begin
+    //             st <= 99;
+    //         end
+    //     end
+    //     //Parse
+    //     else if (st >= 7) begin
+    //         //validate ,'s are where expected
+    //         if ((st == 7) || (st == 18) || (st == 28) || (st == 30) || (st == 41) || (st == 43) || (st == 45) || (st == 47) || (st == 52) || (st == 57) || (st == 59) || (st == 64) || (st == 66) || (st == 70)) begin
+    //             if (!(dataByte ^ 8'b00101100)) begin
+    //                 olst = st;
+    //                 st++;
+    //                 if (st == 7) begin
+    //                     gpgga[0] = dataByte;
+    //                 end
+    //             end else begin
+    //                 st = 99;
+    //             end
+    //         //fill arrays with values
+    //         end else if (st < 82) begin
+    //             if ((st >= 8) && (st < 18)) begin
+    //                 utc[17-st] = dataByte;
+    //             end else if ((st >= 19) && (st < 28)) begin
+    //                 latitude[27-st] = dataByte;
+    //             end else if ((st >= 29) && (st < 30)) begin
+    //                 ns[29-st] = dataByte;
+    //             end else if ((st >= 31) && (st < 41)) begin
+    //                 longitude[40-st] = dataByte;
+    //             end else if ((st >= 42) && (st < 43)) begin
+    //                 ew[42-st] = dataByte;
+    //             end else if ((st >= 44) && (st < 45)) begin
+    //                 quality[44-st] = dataByte;
+    //             end else if ((st >= 46) && (st < 47)) begin
+    //                 numSats[46-st] = dataByte;
+    //             end else if ((st >= 48) && (st < 52)) begin
+    //                 hdop[51-st] = dataByte;
+    //             end else if ((st >= 53) && (st < 57)) begin
+    //                 geoid[56-st] = dataByte;
+    //             end else if ((st >= 58) && (st < 59)) begin
+    //                 metersH[58-st] = dataByte;
+    //             end else if ((st >= 60) && (st < 64)) begin
+    //                 geoidal[63-st] = dataByte;
+    //             end else if ((st >= 65) && (st < 66)) begin
+    //                 metersG[65-st] = dataByte;
+    //             end else if ((st >= 67) && (st < 70)) begin
+    //                 age[69-st] = dataByte;
+    //             end else if ((st >= 71) && (st < 81)) begin
+    //                 checksum[80-st] = dataByte;
+    //             st++;
+    //             //Checksum
+    //             end else begin
+    //                 cSum = gpgga ^ utc ^ latitude ^ ns ^ longitude ^ ew ^ quality ^ numSats ^ dilution ^ altitude ^ metersH ^ geoidal ^ metersG ^ age;
+    //                 //data ready
+    //                 if (!(cSum ^ checksum)) begin
+    //                     GPSReady <= 1;
+    //                 end else begin
+    //                     GPSReady <= 0;
+    //                     st <= 99;
+    //                 end
+    //             end
+    //         end
+    //     end
+    // end
 
 /////////////////////////////////////////////////////////////////////
 //use state machine to track which register to write to
